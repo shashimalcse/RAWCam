c1049366eefed89c6d16672786c948debbe7294b
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2021-05-28 11:45:55 +0530

    Initial Commit

---
+++ RAWCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAWCam
+//
+//  Created by thilina shashimal senarath on 5/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
